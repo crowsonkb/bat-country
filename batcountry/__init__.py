d9ffbe0a3b7fae4915a61fb892eee5573f265a58
Katherine Crowson <crowsonkb@gmail.com>
2016-02-24 18:44:40 -0800

    Pylint for __init__.py

--- batcountry/__init__.py
+++ batcountry/__init__.py
@@ -1,4 +1,8 @@
-__author__ = 'adrianrosebrock'
+"""The 'bat-country' package is an easy to use, highly extendible, lightweight
+Python module for inceptionism and deep dreaming with Convolutional Neural
+Networks and Caffe."""
 
 # import the necessary packages
 from .batcountry import BatCountry
+
+__author__ = 'adrianrosebrock'
